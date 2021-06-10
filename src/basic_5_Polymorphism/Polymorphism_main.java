6df8830b1afe4e935e36a97e1a9bfdf094e5d784
joonghoo <joonghoo@localhost>
2021-06-11 00:12:39 +0900

    study of polymorphism

---
+++ src/basic_5_Polymorphism/Polymorphism_main.java
@@ -0,0 +1,33 @@
+package basic_5_Polymorphism;
+
+class Animal{
+	public void move() {
+		System.out.println("동물이 움직인다.");
+	}
+}
+
+class Human extends Animal{
+	@Override
+	public void move() {
+		System.out.println("사람이 움직인다.");
+	}
+}
+
+class Tiger extends Animal{
+	@Override
+	public void move() {
+		System.out.println("호랑이가 움직인다.");
+	}
+}
+
+public class Polymorphism_main {
+	public static void main(String[] args) {
+		moveAnimal(new Animal());
+		moveAnimal(new Human());
+		moveAnimal(new Tiger());
+	}
+	
+	public static void moveAnimal(Animal animal) {
+		animal.move();
+	}
+}
